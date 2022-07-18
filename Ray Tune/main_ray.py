6a7d252bd0ccc88614d86dac58f36711ac8b10e5
HATON Romain <hatonromain@pop-os.localdomain>
2022-07-18 13:53:53 +0200

    ajout de 6 fichiers .py

---
+++ Ray Tune/main_ray.py
@@ -0,0 +1,16 @@
+from tray2 import *
+from ray import tune
+from preprocess2 import *
+from model2 import *
+from ray.tune.schedulers import ASHAScheduler
+import os
+
+config = {
+    "embedding_dim": tune.grid_search([8,16,32,64,128]),
+		"learning_rate" : 1e-3,
+    "split": tune.grid_search([5, 500, 50000]),
+    "lambda_u": tune.grid_search([0.1, 1, 10]),
+    "lambda_i": tune.grid_search([0.1, 1, 10])
+}
+
+analysis = tune.run(train_ray, num_samples = 1, config=config, resources_per_trial = {"cpu" : 10}, local_dir = "/home/gauthierv/jodie/compte_rendu", verbose = 0)
