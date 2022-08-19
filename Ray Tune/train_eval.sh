3ee60e416ceb1fd016dfcdca021e71b4ea7f2cab
Romain HATON <79025189+HATON-R@users.noreply.github.com>
2022-08-19 11:35:24 +0200

    Update train_eval.sh

--- Ray Tune/train_eval.sh
+++ Ray Tune/train_eval.sh
@@ -4,6 +4,6 @@ while read line;
 do
   for x in $line;
   do
-    python3 evaluate.py --split_select $x
+    python3 evaluate_model.py --split_select $x
   done
 done < /home/gauthierv/jodie/hyper-parameter.txt
