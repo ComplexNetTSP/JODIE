98950524f3eed19eab87d1366bebdfbe7e609548
Vincent Gauthier <pub@luxbulb.org>
2022-08-19 14:42:14 +0200

    add new envs file

--- trainning_example.py
+++ trainning_example.py
@@ -5,8 +5,17 @@ from jodie.train import *
 from ray.tune.schedulers import ASHAScheduler
 import os
 
-# Romain
-config = {
+# Simple config
+config_fast = {
+    "embedding_dim": 8,
+    "learning_rate": 1e-3,
+    "split": 500,
+    "lambda_u": 1,
+    "lambda_i": 1,
+    "dataset": "mooc"
+}
+
+config_long = {
     "embedding_dim": tune.grid_search([8, 16, 32, 64, 128]),
     "learning_rate": 1e-3,
     "split": tune.grid_search([5, 500, 50000]),
@@ -15,10 +24,12 @@ config = {
     "dataset": "mooc"
 }
 
+
 if __name__ == '__main__':
+    print("Start the trainning")
     analysis = tune.run(train_ray,
                         num_samples=1,
-                        config=config,
+                        config=config_fast,
                         resources_per_trial={"cpu": 2},
-                        local_dir="./jodie/result",
+                        local_dir="./result",
                         verbose=0)
